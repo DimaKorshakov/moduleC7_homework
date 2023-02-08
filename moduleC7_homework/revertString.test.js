fc71f738dee03abf98d2fd71f4cbb5a3fe37204c
DimaKorshakov <86188277+DimaKorshakov@users.noreply.github.com>
2023-02-08 22:26:31 +0300

    Add files via upload

---
+++ moduleC7_homework/revertString.test.js
@@ -0,0 +1,5 @@
+import { revertString } from "/Users/Cat/Desktop/WBP/moduleC7_homework/revertString.js";
+
+describe("tests for revertString function", () => {
+    it("should reverse string", () => expect(revertString("abc")).toBe("cba"));
+});
